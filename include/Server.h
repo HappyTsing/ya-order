d6553d6dac598b5f4948c34f466fbc30fffe36c2
happytsing <wangleqing00@gmail.com>
2022-12-17 11:24:25 +0800

    release: Server

--- include/Server.h
+++ include/Server.h
@@ -1,18 +1,24 @@
 #ifndef YA_ORDER_SERVER_H
 #define YA_ORDER_SERVER_H
 #include <string>
+#include <vector>
+#include "Item.h"
 
 using namespace std;
 class Server{
 public:
     int id;
     int port;
-    string ip;
 
 public:
-    Server(int id, int port,string ip);
+    Server(int id, int port);
+    ~Server();
     int start();  // 开始监听
     // 接下来的方法，用于处理客户端的操作。
+    vector<Item> searchAll();
+    void update(int itemID,Item* item);
+    void remove(int itemID);
+    void add(Item* item);
 };
 
 #endif //YA_ORDER_SERVER_H
--- src/Server.cpp
+++ src/Server.cpp
@@ -3,23 +3,23 @@
 #include <unistd.h>
 #include <iostream>
 #include <sys/socket.h>
-#include <arpa/inet.h>
+#include <vector>
+#include <fcntl.h>
 #include "../include/Server.h"
 
-Server::Server(int id, int port, string ip) {
+Server::Server(int id, int port) {
     this->id = id;
     this->port = port;
-    this->ip = ip;
 }
 
 int Server::start() {
     int nListenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 
-    std::cout <<"nListenSocket= " <<nListenSocket << std::endl;
+//    std::cout <<"nListenSocket= " <<nListenSocket << std::endl;
 
     if(-1 == nListenSocket)
     {
-        std::cout << "socket error" << std::endl;
+        perror("socket error");
         return 0;
     }
 
@@ -27,18 +27,20 @@ int Server::start() {
     memset(&ServerAddress, 0, sizeof(sockaddr_in));
     ServerAddress.sin_family = AF_INET;
     ServerAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    ServerAddress.sin_port = htons(5000);
+    ServerAddress.sin_port = htons(this->port);
 
+    // 绑定 ip 和 port
     if(::bind(nListenSocket, (sockaddr *)&ServerAddress, sizeof(sockaddr_in)) == -1)
     {
-        std::cout << "bind error" << std::endl;
+        perror("bind error");
         ::close(nListenSocket);
         return 0;
     }
 
+    // 开启监听
     if(::listen(nListenSocket, 23) == -1)
     {
-        std::cout << "listen error" << std::endl;
+        perror("listen error");
         ::close(nListenSocket);
         return 0;
     }
@@ -47,49 +49,226 @@ int Server::start() {
     sockaddr_in ClientAddress;
     socklen_t LengthOfClientAddress = sizeof(sockaddr_in);
 
-    std::cout<<"before conjuction"<<std::endl;
-    //设置超时时间片6s
-    // struct timeval timeout = {6,0};
-    // int state = 0;
-    // int temp = 0;
-
+    cout << "启动 server 成功, 开始接受连接..." << endl;
     while(true)
     {
-        //超时设置，对IO 套接字进行超时设置，如果超过时间片
-        // if (setsockopt(nListenSocket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(struct timeval)) != 0)
-        // {
-        //     printf("set accept timeout failed");
-        // }
-        //如果accept超时，则accept会返回-1
+        // 接受连接
         int nConnectedSocket = ::accept(nListenSocket, (sockaddr *)&ClientAddress, &LengthOfClientAddress);
         if(-1 == nConnectedSocket)
         {
-            std::cout << "accept error" << std::endl;
+            perror("accept error");
             ::close(nListenSocket);
             //break;
         }
-        ::write(nConnectedSocket, "Server A is running\n", 20);
-
+        cout <<"开始发送数据"<< endl;
+        // 收发数据，用函数send()和recv()，或者read()和write();
+        ::write(nConnectedSocket, "This is message from server A\n", 50);
     }
-    // std::cout<<"Process A has been killed"<<std::endl;
-    // for(int i =0 ;i <1000; i++)
-    // {
-    //     std::cout<<"Process B is running   "<< std::endl;
-    //     sleep(3);
-    // }
-
-    // int nConnectedSocket = ::accept(nListenSocket, (sockaddr *)&ClientAddress, &LengthOfClientAddress);
-    //     if(-1 == nConnectedSocket)
-    //     {
-    //         std::cout << "accept error" << std::endl;
-    //         ::close(nListenSocket);
-    //         return 0;
-    //     }
-    //     ::write(nConnectedSocket, "Process A is running\n", 20);
-
-
-    // ::close(nConnectedSocket);
-    // ::close(nListenSocket);
 
     return 0;
+}
+
+Server::~Server() = default;
+/**
+ * 首先存数量
+ * @return
+ */
+vector<Item> Server::searchAll() {
+    vector<Item> result;
+    string dbName = "db_" + to_string(this->id);
+
+    int fd = open(dbName.c_str(), O_RDWR, 0664);
+
+
+    if (-1 == fd) {
+        perror("server db error open");
+    }
+
+    if (-1 == lseek(fd, 0, SEEK_SET)) {
+        perror("server lseek");
+    }
+    int count;
+    read(fd,&count,4);
+
+    cout << "当前共有 " + to_string(count) + " 个物品"  << endl;
+
+    for(int i = 0; i < count; i++) {
+        // 定位到下一个商品
+        if (-1 == lseek(fd, 4+i*4*3, SEEK_SET)) {
+            perror("server lseek");
+        }
+        int i_id;
+        int i_price;
+        int i_amount;
+        read(fd,&i_id,4);
+        read(fd,&i_price,4);
+        read(fd,&i_amount,4);
+        Item item = Item(i_id, i_price, i_amount);
+        result.push_back(item);
+    }
+
+    if (-1 == close(fd)) {
+        perror("server close");
+    }
+    return result;
+}
+
+void Server::add(Item *item) {
+    string dbName = "db_" + to_string(this->id);
+
+    if (-1 == open(dbName.c_str(), O_RDWR | O_CREAT | O_EXCL, 0664)) {
+        // 表文件已经存在，说明表已经被创建过
+        if (17 == errno) {
+            string msg = "「" + dbName + "」already exists, create fail.";
+            cout << msg << endl;
+        }
+    }else{
+        int fd = open(dbName.c_str(), O_WRONLY | O_CREAT, 0664);
+        if (-1 == fd) {
+            perror("server db error open");
+        }
+        int count = 0;
+        write(fd,&count,4);
+        close(fd);
+    }
+
+    int fd = open(dbName.c_str(), O_RDWR, 0664);
+
+    if (-1 == fd) {
+        perror("server db error open");
+    }
+
+    if (-1 == lseek(fd, 0, SEEK_SET)) {
+        perror("server lseek");
+    }
+    int count;
+    read(fd,&count,4);
+
+    cout << "当前共有 " + to_string(count) + " 个物品"  << endl;
+
+    if (-1 == lseek(fd, 4 + 3*4*count, SEEK_SET)) {
+        perror("server lseek");
+    }
+
+    write(fd,&item->id,4);
+    write(fd,&item->price,4);
+    write(fd,&item->amount,4);
+
+    cout << "add 新物品 成功" << endl;
+
+    if (-1 == lseek(fd, 0, SEEK_SET)) {
+        perror("server lseek");
+    }
+    count ++;
+    write(fd,&count,4);
+
+
+    if (-1 == close(fd)) {
+        perror("server close");
+    }
+
+}
+
+void Server::update(int itemID, Item *item) {
+
+    string dbName = "db_" + to_string(this->id);
+
+    int fd = open(dbName.c_str(), O_RDWR, 0664);
+
+    if (-1 == fd) {
+        perror("server db error open");
+    }
+
+    if (-1 == lseek(fd, 0, SEEK_SET)) {
+        perror("server lseek");
+    }
+    int count;
+    read(fd,&count,4);
+
+    cout << "当前共有 " + to_string(count) + " 个物品"  << endl;
+    for(int i = 0; i < count; i++) {
+        // 定位到下一个商品
+        if (-1 == lseek(fd, 4+i*4*3, SEEK_SET)) {
+            perror("server lseek");
+        }
+        int i_id;
+        read(fd,&i_id,4);
+        if(i_id == itemID){
+            if (-1 == lseek(fd, 4+i*4*3, SEEK_SET)) {
+                perror("server lseek");
+            }
+            write(fd,&item->id,4);
+            write(fd,&item->price,4);
+            write(fd,&item->amount,4);
+            cout << "发现更新目标，更新完成" << endl;
+        }
+    }
+
+    if (-1 == close(fd)) {
+        perror("server close");
+    }
+}
+
+/**
+ * 采取全部前移的方式
+ * @param itemID
+ */
+void Server::remove(int itemID) {
+    string dbName = "db_" + to_string(this->id);
+
+    int fd = open(dbName.c_str(), O_RDWR, 0664);
+
+    if (-1 == fd) {
+        perror("server db error open");
+    }
+
+    if (-1 == lseek(fd, 0, SEEK_SET)) {
+        perror("server lseek");
+    }
+    int count;
+    read(fd,&count,4);
+
+    cout << "当前共有 " + to_string(count) + " 个物品"  << endl;
+    int i;
+    for(i = 0; i < count; i++) {
+        // 定位到下一个商品
+        if (-1 == lseek(fd, 4+i*4*3, SEEK_SET)) {
+            perror("server lseek");
+        }
+        int i_id;
+        read(fd,&i_id,4);
+        if(i_id == itemID){
+            break;
+        }
+    }
+    for(int j = i ; j < count; j++){
+        if (-1 == lseek(fd, 4+(j+1)*4*3, SEEK_SET)) {
+            perror("server lseek");
+        }
+
+        int j_id;
+        int j_price;
+        int j_amount;
+        read(fd,&j_id,4);
+        read(fd,&j_price,4);
+        read(fd,&j_amount,4);
+
+        if (-1 == lseek(fd, 4+j*4*3, SEEK_SET)) {
+            perror("server lseek");
+        }
+        write(fd,&j_id,4);
+        write(fd,&j_price,4);
+        write(fd,&j_amount,4);
+    }
+
+
+    if (-1 == lseek(fd, 0, SEEK_SET)) {
+        perror("server lseek");
+    }
+    count --;
+    write(fd,&count,4);
+
+    if (-1 == close(fd)) {
+        perror("server close");
+    }
 }
